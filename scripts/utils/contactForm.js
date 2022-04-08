a38c6eb7623eea7a188897d4cc0360a28ab98836
ZSchmitt <zaiane.schmitt@gmail.com>
2022-04-08 17:43:44 +0200

    Contact form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,36 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    const $modal = document.getElementById("bground-opacity");
+    const $mainHidden = document.getElementById("main");
+    const $contactForm = document.getElementById("contactform");
+    const $closeBtn = document.getElementById("close-modal-btn");
+    
+
+	$modal.style.display = "block";
+    $mainHidden.setAttribute("aria-hidden", "true");
+    $contactForm.setAttribute("aria-hidden", "false");
+    $closeBtn.focus();
+    
+    // Close modal when espace key is pressed
+document.addEventListener('keydown', event => {
+    const keyCode = event.keyCode ? event.keyCode : event.which
+  
+    if ($contactForm.hasAttribute("aria-hidden", "false") && keyCode === 27) {
+        closeModal()
+    }
+ })
+
+
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+    const $modal = document.getElementById("bground-opacity");
+    const $mainHidden = document.getElementById("main");
+    const $contactForm = document.getElementById("contactform");
+    const $contactBtn = document.querySelector(".contact-btn");
+
+    $modal.style.display = "none";
+    $mainHidden.setAttribute("aria-hidden", "false");
+    $contactForm.setAttribute("aria-hidden", "true");
+    $contactBtn.focus()
 }
+
