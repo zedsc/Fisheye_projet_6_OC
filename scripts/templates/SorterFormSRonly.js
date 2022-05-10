69f3d8415e52f86c0b00212b0ded134ff854f667
ZSchmitt <zaiane.schmitt@gmail.com>
2022-05-10 16:54:50 +0200

    Minor modifications
    Regarding classes names (dropdown box)

--- scripts/templates/SorterFormSRonly.js
+++ scripts/templates/SorterFormSRonly.js
@@ -6,7 +6,7 @@ class SorterFormSRonly {
         this._Medias = Medias
 
         this.$mediaSection = document.querySelector('.media-section')
-        this.$sorterContainer = document.querySelector('.filter-container-SR-only')
+        this.$sorterContainer = document.querySelector('.filter-container-sr' + '.sr-only')
     }
 
     // Gets new sorted array and fills the media section with sorted cards
@@ -60,23 +60,23 @@ class SorterFormSRonly {
    render() {
         const sorterForm = `  
             <form class="sorter-form sr-only">
-                <label class="sorter-label" for="sorter-form sr-only">Trier par</label>
-                <div class="select" tabindex="0" role="button" aria-haspopup="listbox" aria-expanded="false">
+                <label class="sorter-label-sr" for="sorter-form sr-only">Trier par</label>
+                <div class="select" role="button" aria-haspopup="listbox" aria-expanded="false">
                     <svg class="sorter-arrow" width="16" height="10" viewBox="0 0 16 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 1.33447L8.00465 8.13311L14.5973 1.33447" stroke="#E9E9E9" stroke-width="2"/>
                     </svg>
         
                     <input aria-checked="true" value="date DESC" class="selectopt" name="opt1" type="radio" id="opt1" checked>
                     <label for="opt1" class="option">
-                        <span class="border-top"></span>
+                        <span class="border-top-sr"></span>
                     Date</label>
                     <input aria-checked="false" value="likes DESC" class="selectopt" name="opt2" type="radio" id="opt2">
                     <label for="opt2" class="option">
-                        <span class="border-top"></span>
+                        <span class="border-top-sr"></span>
                     Popularité</label>
                     <input aria-checked="false" value="title ASC" class="selectopt" name="opt3" type="radio" id="opt3">
                     <label for="opt3" class="option">
-                        <span class="border-top"></span>
+                        <span class="border-top-sr"></span>
                     Titre</label>
                 </div>
             </form>
